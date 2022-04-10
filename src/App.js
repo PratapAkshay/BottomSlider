2af4935978389ef380c30665e137eef2d8d5fb68
PratapAkshay <45003568+PratapAkshay@users.noreply.github.com>
2022-04-11 01:00:22 +0530

    Slide code added

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import { useState } from 'react';
+import './App.css';
+import image from './images/arrow-down-sign-to-navigate.png'
+
+function App() {
+
+  const [show,setShow] = useState(false);
+
+  const changeState = () => {
+    setShow((prev) => {
+      return !prev
+    })
+  }
+
+  return (
+    <div className="App">
+      Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur 
+      Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur 
+      Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur 
+      Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur 
+      Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur 
+      Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur 
+      Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur 
+      Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur 
+      Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur 
+      Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur 
+      Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur 
+      Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur 
+      Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur 
+      Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur 
+      Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur 
+      Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur 
+      Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur 
+      Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur 
+      Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur 
+      Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur 
+      Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur 
+      Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur 
+      Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur 
+      Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur 
+      Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur 
+      Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur 
+      Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur 
+      Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur 
+      Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur 
+      Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur 
+      Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur 
+      Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur 
+      Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur 
+      Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur 
+      Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur 
+      Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur 
+      Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur 
+      Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur 
+      Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur 
+      Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur 
+      Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur 
+      Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur 
+      Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur 
+      Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur 
+      Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur 
+      Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur 
+      Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur 
+      Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur Slide Up/down example representation by Akshay Pratap Singh Thakur 
+      
+      <div className='showHideContainer'>
+        <div className='containerHead'><img onClick={()=>changeState()} className={`imageStyle ${show ? "iconRotate" : ""}`} src={image}/></div>
+        <div className={`containerBody ${show ? 'showContainer' : ''}`}>Hello</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
